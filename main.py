9ac6ff744e0230f3b01f43e2613f3f17ec4e6dbc
Jeffer Zhang <zhangjeffer@gmail.com>
2020-02-26 15:07:30 -0500

    inital flask commit

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, World!"
+
+@app.route("/game")
+def game_html():
+    return render_template("game.html")
+
+@app.route("/login")
+def login_html():
+    return render_template("login.html")
+
+@app.route("/lobby")
+def bench():
+    print('hello')
+    return render_template("lobby.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
