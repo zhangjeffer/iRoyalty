7f7187df6bfdcbc939df01a91f265192fcbfb552
Jeffer Zhang <zhangjeffer@gmail.com>
2020-03-04 15:56:47 -0500

    socket factored into seperate script

--- iRoyalty/app.js
+++ iRoyalty/app.js
@@ -23,19 +23,5 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-// Add the WebSocket handlers
-io.on('connection', function(socket) {
-});
-
-setInterval(function() {
-    io.sockets.emit('message', 'hi!');
-}, 1000);
-
-var players = {};
-
-io.on('connection', function(socket) {
-  socket.on('new player', function(data) {
-    console.log('new user: ' + data);
-    players[socket.id] = date['userId']
-  });
-});
+var socketServer = require("./socket");
+socketServer(io);
---
+++ iRoyalty/socket.js
@@ -0,0 +1,17 @@
+exports = module.exports = function(io){
+        io.on('connection', function(socket) {
+        });
+
+        setInterval(function() {
+            io.sockets.emit('message', 'hi!');
+        }, 1000);
+
+        var players = {};
+
+        io.on('connection', function(socket) {
+        socket.on('new player', function(data) {
+            console.log('new user: ' + data);
+            players[socket.id] = date['userId']
+        });
+    });
+}
