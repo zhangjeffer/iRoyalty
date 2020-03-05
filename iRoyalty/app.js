b2ec91fe4a9126a550381c6b79b4f054861628db
Jeffer Zhang <zhangjeffer@gmail.com>
2020-03-04 21:25:03 -0500

    socket factored out

--- iRoyalty/app.js
+++ iRoyalty/app.js
@@ -24,18 +24,5 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-// Add the WebSocket handlers
-io.on('connection', function(socket) {
-});
-
-setInterval(function() {
-    io.sockets.emit('message', 'hi!');
-}, 1000);
-
-var players = {};
-
-io.on('connection', function(socket) {
-  socket.on('new player', function(data) {
-    console.log('new user: ' + data);
-  });
-});
+var socketServer = require("./socket");
+socketServer(io);
