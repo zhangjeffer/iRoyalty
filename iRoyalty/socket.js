ddf17ada514ad26a2dbedf06044d50a2c370a644
Jeffer Zhang <zhangjeffer@gmail.com>
2020-03-06 10:20:31 -0500

    huy's changes

--- iRoyalty/socket.js
+++ iRoyalty/socket.js
@@ -1,16 +1,51 @@
 exports = module.exports = function(io){
-        io.on('connection', function(socket) {
-        });
+        var room_counter = 1;
 
         setInterval(function() {
             io.sockets.emit('message', 'hi!');
         }, 1000);
 
-        var players = {};
+        var socketid_username = {};
+        var username_socketid = {};
 
         io.on('connection', function(socket) {
-        socket.on('new player', function(data) {
-            console.log('new user: ' + data);
-        });
+            socket.on('new player', function(username) {
+                socketid_username[socket.id] = username;
+                username_socketid[username] = socket.id;
+                console.log(username + " registered with socket.id: " + socket.id);
+            });
+
+            socket.on('find_opponent', function(opponent) {
+                console.log("find " + opponent);
+                if (username_socketid[opponent]) {
+                    io.to(username_socketid[opponent]).emit('challenger', socketid_username[socket.id]);
+                }
+            });
+
+            socket.on('establish_pairing', function(opponent) {
+                
+                var roomname = "room" + room_counter;
+                socket.join(roomname);
+                io.sockets.connected[username_socketid[opponent]].join(roomname);
+                room_counter += 1; 
+                var gameinfo = {
+                    room: roomname,
+                    player1: opponent,
+                    player2: socketid_username[socket.id]
+                };
+                io.to(roomname).emit('startgame', gameinfo);
+
+            });
+
+            socket.on('sendreject', function(opponent) {
+                io.to(username_socketid[opponent]).emit('reject', socketid_username[socket.id]);
+            });
+
+            socket.on("move piece", function(data) {
+                console.log(data);
+                var move = socketid_username[socket.id] + ": " + data.inputmove;
+                io.to(data.room).emit('update board', move);
+            });
+
     });
 }
