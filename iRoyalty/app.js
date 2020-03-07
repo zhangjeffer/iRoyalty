c5b37e2cfed19a357c7292a0beb778246e5f4641
Jeffer Zhang <zhangjeffer@gmail.com>
2020-03-07 01:23:34 -0500

    prettified app.js

--- iRoyalty/app.js
+++ iRoyalty/app.js
@@ -4,10 +4,12 @@ const bodyParser = require("body-parser");
 const errorHandler = require("./helperz/error-handler");
 const http = require('http');
 const path = require('path');
-const socketIO = require('socket.io');var app = express();
+const socketIO = require('socket.io');
+const app = express();
 const server = http.Server(app);
-const io = socketIO(server);app.set('port', 5000);
-
+const socketServer = require("./socket");
+const io = socketIO(server);
+app.set('port', 5000);
 app.use('/static', express.static(__dirname + '/static'));// Routing
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -19,9 +21,12 @@ server.listen(5000, () =>{
     console.log( "Connecting on port 2424" )  
 });
 
+app.get("/chess",function(req, res){
+    res.sendFile(__dirname + "/chess.html")
+});
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-var socketServer = require("./socket");
 socketServer(io);
