0045d8b0a11b92a6380acc782f2da1f3b99cf14d
Brian Tran <brian@Brians-MBP.fios-router.home>
2020-03-09 01:28:36 -0400

    added new tests

--- iRoyalty/unit-tests/test.js
+++ iRoyalty/unit-tests/test.js
@@ -1,9 +1,11 @@
 //unit testing for socket functions
 //tests strings and json
 "use strict"
-
+const URI = "mongodb+srv://cluster0-sz1zy.mongodb.net/test?retryWrites=true&w=majority";
 const expect = require("chai").expect;
 const io = require("socket.io-client");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 const ioSettings = {
     transports: ["websocket"],
     forceNew: true,
@@ -16,6 +18,67 @@ const jsonTest = {
 var server;
 var emitterZ;
 var listenerZ;
+
+const tSchema = new Schema({
+    userId: {type: String, required: true}
+});
+
+const tUserId = mongoose.model("userId", tSchema);
+
+describe("Mongodb Tests",function(){
+    before(function (done){
+        mongoose.connect(URI,{
+            dbName: "unitTests",
+            user: "Group69",
+            pass: "Group69!",
+            userNewUrlParser: true,
+            useUnifiedTopology: true
+        }, function(error){
+            if(error){
+                console.log(error);
+            }else{
+                console.log("Connected to the db");
+            }
+        });
+        const db = mongoose.connection;
+        db.once("open",function(){
+            done();
+        });
+    });
+    describe("Test Mongodb",function(){
+        it("Adding userID to db",function(done){
+            var testVal = tUserId({
+                userId: "test"
+            });
+            testVal.save(done);
+        });
+        it("adding incorrect format to db", function(done){
+            var wrongFormat = tUserId({
+                wrong: "test"
+            })
+            wrongFormat.save(err => {
+                if(err){
+                    return done();
+                }
+            });
+        });
+        it("removing existing userID from db",function(done){
+            var testVal = tUserId({
+                userId: "test"
+            });
+            testVal.save();
+            testVal.remove(done);
+        });
+        it("removing non existent userID from db",function(done){
+            var testVal = tUserId({
+                userId: "test"
+            });
+            testVal.remove(done);
+        });
+
+    });
+
+});
 
 describe("Socket Functions", function(){
     this.timeout(10000);
